a8b52284488c07ebd4d8a8931ee7285bbf9a5e20
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-11-18 05:56:08 +0000

    Added a new file

--- print_alphabet.c
+++ print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(alp[i]);
+		putchar(alpha[i]);
 	}
 	putchar('\n');
 	return (0);
