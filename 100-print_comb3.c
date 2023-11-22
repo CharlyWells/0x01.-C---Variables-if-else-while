ef8f18382dc689eb618c8e6c007c3f976c3e3e50
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-11-22 16:42:32 +0000

    Added a new file

---
+++ 100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * description: A program that prints all possible different combinations of two digits
+ * numbers must be seperated by , followed by a space
+ * return 0
+ */
+int main()
+{
+  int num;
+  for(num = 48; num <= 57; num++)
+     {
+       putchar(num + '0');
+     }
+  for(num = 
+}
